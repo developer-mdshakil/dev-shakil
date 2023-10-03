e4d8d3e87468dac4138906cf356a5fa5232939cf
developer-mdshakil <developershakil.cmt1819@gmail.com>
2023-10-03 15:44:53 +0600

    react router setup in single page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routers/Router';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="">
+      <RouterProvider router={router} />
     </div>
   );
 }
